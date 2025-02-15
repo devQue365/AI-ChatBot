aca29a847cf4f5aa8b927388049d0b6f33cbded2
devQue365 <devanshr2810@gmail.com>
2025-02-15 14:19:51 +0530

    Added offline as well as Online text-to-speech mechansims

---
+++ func/SpeakOffline.py
@@ -0,0 +1,19 @@
+import pyttsx3 as tts
+
+# Initialize engine
+engine = tts.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice',voices[0].id)
+engine.setProperty('rate',200)
+
+def Speak(*args, **kwargs):
+    # kwargs to be used in future
+    audio = ""
+    for i in args: # tuple
+        audio += str(i)
+    print(f'\nRete(X) : [\'{audio}\b\']\n')
+    engine.say(audio)
+    engine.runAndWait()
+if __name__ == '__main__':
+    Speak('Hi','my','name','is','Maruti')
---
+++ func/SpeakOnline.py
@@ -0,0 +1,34 @@
+# using selenium module
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from time import sleep
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless') # don't show chrome interface to user
+chrome_options.headless = True
+
+driver = webdriver.Chrome(options=chrome_options)
+website = r'https://ttsmp3.com/text-to-speech/British2English/'
+driver.get(website)
+
+ButtonSelection = Select(driver.find_element(by=By.ID, value = "sprachwahl"))
+ButtonSelection.select_by_visible_text("British English / Brian")
+
+def Speak(*args):
+    text = ""
+    for i in args:
+        text+= (str(i)  + ' ')
+    
+    lengthOfText = len(str(text))
+    if(lengthOfText == 0):
+        pass
+    else:
+        print(f'\nRete(X) : [\'{text}\b\']\n')
+        push_data = str(text)
+        driver.find_element(By.ID, value = "voicetext").send_keys(push_data)
+        driver.find_element(By.ID, value = "vorlesenbutton").click()
+        # clear the voicetext area
+        driver.find_element(By.ID, value = "voicetext").clear()
+
+Speak('a',1,2.34,5)
