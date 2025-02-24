f062232964c02f06ad4a3a1604e02cafbacdb842
devQue365 <devanshr2810@gmail.com>
2025-02-24 17:48:54 +0530

    Added a wait/pause feature once SR ends

--- func/ListenJs.py
+++ func/ListenJs.py
@@ -1,25 +1,36 @@
-from selenium import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-options = webdriver.ChromeOptions()
+import undetected_chromedriver as uc
+# options = webdriver.ChromeOptions()
+options = uc.ChromeOptions()
 options.add_argument("--use-fake-ui-for-media-stream")
-# options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
+options.add_argument("--headless=new")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-software-rasterizer")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = uc.Chrome(options=options)
 url = r"C:\Users\Hp\Desktop\Projects\projX\projX\data\voice.html"
 
 # get the url
-driver.get(url)
+# driver.get(url)
+try:
+    driver.get(url)
+except Exception as e:
+    print(f"Error loading page: {e}")
 
 def Listen():
     print("Listening ...")
     driver.find_element(By.ID, value = 'start').click()
     while True:
-        output = driver.find_element(By.ID, value = 'output')
-        if output.text != "":
-            print(f"You said : {output.text}")
+        output = driver.find_element(By.ID, value = 'output').text
+        if output != "":
+            print(f"You said : {output}")
             driver.find_element(By.ID, value = 'end').click()
+            # driver.service.stop()
             return output
 
-while True:
-    Listen()
+# while True:
+#     Listen()
 
