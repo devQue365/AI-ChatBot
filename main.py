8f9c6dd8ad91fb57cc1c5c3b4ebf050b8d722aac
devQue365 <devanshr2810@gmail.com>
2025-03-14 20:05:59 +0530

    Update main code

--- main.py
+++ main.py
@@ -16,36 +16,47 @@ if(__name__ == '__main__'):
         QL = query.lower()
         len_Q = len(query.split(" "))
         start_Q = QL.split(" ")[0]
-        end_Q = query.split(" ")[-1]
+        # end_Q = query.split(" ")[-1]
 
-        # Check for OCR request
-        if(start_Q == "click" or (start_Q == "double" and "click" in query)) and len_Q<7:
-            # remove unwanted chunks
-            QL = QL.replace("click","").replace("on","").replace("sam", "").replace("double", "")
-            if(start_Q == "double"):
-                response = ocr_v1_clk(QL.strip(),double_click=True)
-            else:
-                response = ocr_v1_clk(QL.strip())
-
+        # Send the query first to GPT
+        response = SAM(query+"*** Reply in brief short way like SAM without telling the user ***")
+        if(response != None):
             genEffect('\033[1m\033[33m'+response+'\033[0m')
             Speak(response)
-        elif("bob" in start_Q):
-            _code = SAM(query, "*** Use python programming language. Just write complete code nothing else")
-            _code = Filter(_code)
-            exec(_code)
-        elif("search internet" in QL):
-            QL =  re.sub(r'^.*?\bfor\b\s*', '', QL, flags=re.IGNORECASE)
-            response = search_google(QL.strip())
-            genEffect('\033[1m\033[33m'+response+'\033[0m')
-            Speak(response)
-        else:    
-            response = SAM(query+"*** Reply in brief short way like SAM without telling the user ***")
-            if response:
-                genEffect('\033[1m\033[33m'+response+'\033[0m')
-                Speak(response)
-            elif Chat(QL)[1]>0.99:
-                response  = Chat(QL)[0]
-                genEffect('\033[1m\033[33m'+response+'\033[0m')
-                Speak(response)
+        else:
+            # check for OCR
+            if(("click" in QL) or ("double" and "click" in QL)):
+                def extract_clickable_elements(text):
+                    # used regex to match clickable elements
+                    pattern = r'(?i)(?:click on\s+)([A-Za-z0-9\s]+?)(?=\s*(?:and|,|\.|$))'
+                    # Extract clickable elements
+                    elements = re.findall(pattern, text)
+                    return [element.strip() for element in elements]
+                clickables = extract_clickable_elements(QL)
+                # print(clickables)
+                os.system("clear")
+                for clickable in clickables:
+                    response = ocr_v1_clk(clickable)
+                    genEffect('\033[1m\033[33m'+response+'\033[0m')
+                    Speak(response) 
+            elif("bob" in start_Q):
+                QL = QL.replace('bob','')
+                _code = SAM(QL, "*** Use python programming language. Just write complete code nothing else")
+                _code = Filter(_code)
+                exec(_code)
+        # elif("search internet" in QL):
+        #     QL =  re.sub(r'^.*?\bfor\b\s*', '', QL, flags=re.IGNORECASE)
+        #     response = search_google(QL.strip())
+        #     genEffect('\033[1m\033[33m'+response+'\033[0m')
+        #     Speak(response)
+        # else:    
+        #     response = SAM(query+"*** Reply in brief short way like SAM without telling the user ***")
+        #     if response:
+        #         genEffect('\033[1m\033[33m'+response+'\033[0m')
+        #         Speak(response)
+        #     elif Chat(QL)[1]>0.99:
+        #         response  = Chat(QL)[0]
+        #         genEffect('\033[1m\033[33m'+response+'\033[0m')
+        #         Speak(response)
         os.system("pause")
     
